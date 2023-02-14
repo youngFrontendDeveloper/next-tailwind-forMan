d2424255f51d87d60485ca0457870e27c1167d01
Nina <frontend-developer@inbox.ru>
2023-02-14 17:08:17 +0300

    Изменени е названия

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,21 +1,36 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + '- Amazona' : 'Amazona'}</title>
-        <meta name="description" content="Ecommerce Website" />
+        <title>{title ? title + '- ForChild' : 'ForChild'}</title>
+        <meta name="description" content="Ecommerce Website ForChild" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
-          <div></div>
+          <nav className="flex h-12 items-center justify-between shadow-md px-4">
+            <Link href="/" className="text-lg font-bold">
+              ForChild
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
         </header>
-        <main>{children}</main>
-        <footer>Footer</footer>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Copyright 2022 ForChild
+        </footer>
       </div>
     </>
   );
