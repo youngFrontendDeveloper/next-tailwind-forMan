aa71577f7db70ee0050a6e81561c202cb1249b8b
Nina <frontend-developer@inbox.ru>
2023-02-14 17:14:44 +0300

    Добавлен копирайт в футер

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -28,8 +28,9 @@ function Layout({ title, children }) {
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
-          Copyright 2022 ForChild
+        <footer className="flex h-10 justify-between items-center shadow-inner px-4">
+          <p>Copyright &copy; 2022</p>
+          <p>e-shop &quot;ForChild&quot;</p>
         </footer>
       </div>
     </>
